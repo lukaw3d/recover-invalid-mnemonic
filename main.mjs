ade0b19507e21018375c0b0aa58dbf0ea56e97a7
lukaw3d <lukajeran@oasislabs.com>
2021-12-04 03:25:09 +0100

    Generate possible mnemonics

---
+++ main.mjs
@@ -0,0 +1,27 @@
+const bip39 = require('bip39');
+const allWords = bip39.wordlists[bip39.getDefaultWordlist()];
+
+window.form.addEventListener('submit', event => {
+  try {
+    event.preventDefault();
+
+    const partialMnemonic = window.partialMnemonic.value
+      .trim()
+      .replace(/\n/g, ' ')
+      .replace(/ +/g, ' ');
+
+    if (partialMnemonic.split('?').length - 1 < 1) throw new Error('No missing word marked with "?" in mnemonic');
+    if (partialMnemonic.split('?').length - 1 > 1) throw new Error('Mnemonic should only contain one missing word marked with "?"');
+    if (partialMnemonic.split(' ').length % 3 !== 0) throw new Error('Mnemonic length is invalid');
+
+    const possibleMnemonics = allWords
+      .map(w => partialMnemonic.replace('?', w))
+      .filter(m => bip39.validateMnemonic(m));
+
+    if (possibleMnemonics.length <= 0) throw new Error('Mnemonic is invalid - probably contains invalid words or has invalid length');
+
+    window.out.textContent = `Possible mnemonics:\n${possibleMnemonics.join('\n')}`;
+  } catch (err) {
+    window.out.textContent = err;
+  }
+});
